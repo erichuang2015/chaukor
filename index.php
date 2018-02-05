44eeb2f78c994c3ac5d41c9a214903efcba4dfcf
Michael B <michael@canitia.nl>
2018-02-05 16:29:27 +0100

    tweaking and more tweaking

--- index.php
+++ index.php
@@ -3,24 +3,13 @@
 ?>
 <div class="blocktainer col-md-12 col-lg-12" >
  
-<?php if ( !get_theme_mod( 'display_featured_content' ) == 'showslider') : ?>
- <div class="image-side hidden-sm-up col-sm-12">
-<?php  if ( has_post_thumbnail() ) {?>
-
- <?php the_post_thumbnail('full', ['class' => 'img-fluid', 'title' => 'Feature image']);?>
-          
- <?php } else { ?>		
-              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" width="3840" class="d-block img-fluid wp-post-image" />		
-              <?php }  ?>
- </div>  
-  <?php  endif;  ?>
- 
 <?php
+
       $colorcounter = 0;
       if ( have_posts() ) : 
         while ( have_posts() ) : the_post(); ?>
-          <div class="block-item <?php if($colorcounter == 1) { echo 'even-color'; } else { echo 'uneven-color'; } ?>">
-            <div class="text-side col-sm-12 col-md-5 col-lg-6">
+          <div class="block-item">
+          <div class="text-side col-sm-12 col-md-5 col-lg-6">
                 <h1 title="<?php the_title_attribute(); ?>" class="main-title"><?php the_title(); ?></h1>
                 <p><?php the_excerpt(); ?></p>
                  <a class="btn btn-primary readmore-btn" href="<?php the_permalink(); ?>" role="button" title="<?php the_title_attribute(); ?>" ><?php _e('Read more', 'chaukor'); ?></a>
@@ -46,7 +35,9 @@
           }
 
 ?>
-          <?php endwhile; else: ?>
+<?php endwhile;
+        
+        else: ?>
           <div class="post-content">
               <p><?php _e('Sorry, it seems there are no posts available.', 'chaukor'); ?></p>
               <?php get_search_form(); ?>
