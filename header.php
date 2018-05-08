07ad11669c5f4f6f84f0df78cc157538be2e75df
Michael Boumann <git-public@canitia.nl>
2018-05-08 23:55:23 +0200

    introducing - timeline

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 
 <nav class="navbar navbar-toggleable-sm <?php if ( is_admin_bar_showing() ) { echo 'admin-nav'; } ?>" data-toggle="sticky-onscroll">
         <?php if ( function_exists( 'the_custom_logo' ) ) {  the_custom_logo(); }  ?>
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
--- index.php
+++ index.php
@@ -8,34 +8,21 @@
       $colorcounter = 0;
       if ( have_posts() ) : 
         while ( have_posts() ) : the_post(); ?>
+
+
+
           <div class="block-item">
-          <div class="text-side col-sm-12 col-md-5 col-lg-6">
-                <h1 title="<?php the_title_attribute(); ?>" class="main-title"><?php the_title(); ?></h1>
+          <div class="text-side col-md-6 col-lg-6">
+                <h1 title="<?php the_title_attribute(); ?>" class="main-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                 <p><?php the_excerpt(); ?></p>
-                 <a class="btn btn-primary readmore-btn" href="<?php the_permalink(); ?>" role="button" title="<?php the_title_attribute(); ?>" ><?php _e('Read more', 'chaukor'); ?></a>
-                <span class="postmeta"><p class="postdate">
-                    <i class="far fa-clock"></i><time datetime="<?php echo get_the_date('c'); ?>"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp')); echo '&nbsp;'; _e('ago', 'chaukor'); ?></time>
-                </span>
-                
-                </p>         
-                </div>            
-            <div class="image-side col-md-7 col-lg-6 hidden-sm-down ">
-              <?php
-              if ( has_post_thumbnail() ) {
-                  the_post_thumbnail('full', ['class' => 'img-fluid', 'title' => 'Feature image']);
-              }
-              else { ?>		
-              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" width="3840" class="d-block img-fluid wp-post-image" />		
-              <?php }  ?>
+            </div>            
+            <div class="image-side col-md-6 col-lg-6 hidden-sm-down ">
+                     <span class="postmeta"><p class="postdate">
+                        <i class="far fa-clock"></i><time datetime="<?php echo get_the_date('c'); ?>"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp')); echo '&nbsp;'; _e('ago', 'chaukor'); ?></time>
+                    </span>
             </div>
           </div>
-          <?php $colorcounter++; 
-          if ($colorcounter == 3) {
-            $colorcounter = 1;
-          }
-
-?>
-<?php endwhile;
+        <?php endwhile;
         
         else: ?>
           <div class="post-content">
