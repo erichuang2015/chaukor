901f623d87ec4ee8eecbc85b1a905705fd86fefb
Michael Boumann <git-public@canitia.nl>
2018-05-09 10:31:22 +0200

    mobile fixes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <h1 title="<?php the_title_attribute(); ?>" class="main-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                 <p><?php the_excerpt(); ?></p>
             </div>            
-            <div class="image-side col-md-6 col-lg-6 hidden-sm-down ">
+            <div class="image-side col-md-6 col-lg-6 d-none d-sm-none d-md-block">
                      <div class="postmeta"><p class="postdate">
                         <i class="far fa-clock"></i><time datetime="<?php echo get_the_date('c'); ?>"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp')); echo '&nbsp;'; _e('ago', 'chaukor'); ?></time>
                         <br />
