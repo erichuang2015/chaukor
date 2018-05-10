7774e0ef866f7e38737da7fbd024bbc5fabd333b
Michael Boumann <git-public@canitia.nl>
2018-05-10 16:06:50 +0200

    update to fix infinitescroll content being incomplete

--- content.php
+++ content.php
@@ -10,10 +10,17 @@
                 <h1 title="<?php the_title_attribute(); ?>" class="main-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                 <p><?php the_excerpt(); ?></p>
             </div>            
-            <div class="image-side col-md-6 col-lg-6 hidden-sm-down ">
-                     <span class="postmeta"><p class="postdate">
+            <div class="image-side col-md-6 col-lg-6 d-none d-sm-none d-md-block">
+                     <div class="postmeta"><p class="postdate">
                         <i class="far fa-clock"></i><time datetime="<?php echo get_the_date('c'); ?>"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp')); echo '&nbsp;'; _e('ago', 'chaukor'); ?></time>
-                    </span>
+                        <br />
+                        <i class="far fa-user"></i><?php the_author_meta('user_nicename');?>
+                        <?php if ( get_theme_mod( 'show_tags' ) == 'showtags' ) : ?>
+                        <p class="tagslist">
+                            <?php the_tags( '<i class="fas fa-tags" aria-hidden="true"></i>', ' ', ' ' ); ?> 
+                            <?php endif; ?>
+                        </p>
+                      </div>
             </div>
           </div>
         <?php endwhile;
